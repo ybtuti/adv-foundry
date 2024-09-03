5ae03977708e5945fb3d255940a181e15155e5f6
ybtuti <samuelmsuu@gmail.com>
2024-09-03 17:17:10 +0300

    chore: Update Forge Standard Library and ERC-20 contracts

--- erc-20/script/DeployOurToken.s.sol
+++ erc-20/script/DeployOurToken.s.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {OurToken} from "../src/OurToken.sol";
 
-contract CounterScript is Script {}
+contract DeployOurToken is Script {
+    uint256 public constant INITIAL_SUPPLY = 10000 ether;
+
+    function run() external returns (OurToken) {
+        vm.startBroadcast();
+        OurToken ourToken = new OurToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+        return ourToken;
+    }
+}
