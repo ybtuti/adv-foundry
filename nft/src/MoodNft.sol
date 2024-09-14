b92b61d6bc96f1ae6df38b80c5a959f56672b0a4
ybtuti <samuelmsuu@gmail.com>
2024-09-14 14:29:38 +0300

    feat: Add MoodNft contract with happy and sad SVG images

---
+++ nft/src/MoodNft.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract MoodNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
+
+    constructor(
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
+    ) ERC721("MoodNft", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {}
+}
