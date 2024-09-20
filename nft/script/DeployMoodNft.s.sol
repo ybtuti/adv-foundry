7257337d94e49c1a576319e78533104990c80f1d
ybtuti <samuelmsuu@gmail.com>
2024-09-20 11:53:45 +0300

    refactor: Update file paths in foundry.toml and DeployMoodNft.s.sol
    This commit updates the file paths in the `foundry.toml` and `DeployMoodNft.s.sol` files. The path for the images directory in `foundry.toml` has been changed from "./images/" to "./img/". In `DeployMoodNft.s.sol`, the script now reads the SVG files from the "./img/dynamicNft/" directory instead of "./img/". This change ensures that the correct files are accessed and used in the deployment process.

--- nft/script/DeployMoodNft.s.sol
+++ nft/script/DeployMoodNft.s.sol
@@ -2,12 +2,23 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {MoodNft} from "src/MoodNft.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
-    function run() external returns (MoodNft) {}
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./img/dynamicNft/sad.svg");
+        string memory happySvg = vm.readFile("./img/dynamicNft/happy.svg");
+
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(
+            svgToImageURI(sadSvg),
+            svgToImageURI(happySvg)
+        );
+        vm.stopBroadcast();
+        return moodNft;
+    }
 
     function svgToImageURI(
         string memory svg
