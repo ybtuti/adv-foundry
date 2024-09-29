23d66972f286c4fc537ae7293c1d94e802a2d57a
ybtuti <samuelmsuu@gmail.com>
2024-09-29 17:35:39 +0300

    refactor: Update MerkleAirdrop contract with SafeERC20 and MerkleProof imports, error handling, and additional functions

--- merkle-airdrop/src/MerkleAirdrop.sol
+++ merkle-airdrop/src/MerkleAirdrop.sol
@@ -2,7 +2,48 @@
 
 pragma solidity ^0.8.24;
 
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract MerkleAirdrop {
-//Some list of addresses
-//allow someone in the list to claim a ERC-20 token
+    using SafeERC20 for IERC20;
+    //Some list of addresses
+    //allow someone in the list to claim a ERC-20 token
+
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
+
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+    mapping(address claimer => bool claimed) private s_hasClaimed;
+
+    event Claim(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        // Calculate using the account and the amount, the hash which is going to be the leaf node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        s_hasClaimed[account] = true;
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
 }
