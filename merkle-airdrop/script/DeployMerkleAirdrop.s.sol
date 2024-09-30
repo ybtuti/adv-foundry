a999bed716467610460a4915e94a4680ca05e1f6
ybtuti <samuelmsuu@gmail.com>
2024-09-30 19:53:24 +0300

    refactor: Update MerkleAirdrop contract with new merkle root and transfer amount

--- merkle-airdrop/script/DeployMerkleAirdrop.s.sol
+++ merkle-airdrop/script/DeployMerkleAirdrop.s.sol
@@ -8,7 +8,7 @@ import {BagelToken} from "src/BagelToken.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DeployMerkleAirdrop is Script {
-    bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    bytes32 private s_merkleRoot = 0x29bf1ab652621378cc5ec771cf4f4c469079675de2b146c1db1baffc3db9d548;
     uint256 private s_amountToTransfer = 4 * 25 * 1e18;
 
     function deployMerkleAirdrop() public returns (MerkleAirdrop, BagelToken) {
