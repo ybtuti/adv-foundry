42cb021bbd60197b5ec2efd3ee3b375834502add
ybtuti <samuelmsuu@gmail.com>
2024-09-30 19:53:29 +0300

    refactor: Update whitelist addresses in GenerateInput script

--- merkle-airdrop/script/GenerateInput.s.sol
+++ merkle-airdrop/script/GenerateInput.s.sol
@@ -16,8 +16,8 @@ contract GenerateInput is Script {
     function run() public {
         types[0] = "address";
         types[1] = "uint";
-        whitelist[0] = "0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D";
-        whitelist[1] = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+        whitelist[0] = "0x88f0807bF33B15BdF234fa2c92B469F665881BCe";
+        whitelist[1] = "0x67cE8bb909F266AFC7F27BE8a3302C18a7f5Ca14";
         whitelist[2] = "0x2ea3970Ed82D5b30be821FAAD4a731D35964F7dd";
         whitelist[3] = "0xf6dBa02C01AF48Cf926579F77C9f874Ca640D91D";
         count = whitelist.length;
