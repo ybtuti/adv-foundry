c3e1e7c0ca49e039c465ea514834d7dcf32c5892
ybtuti <samuelmsuu@gmail.com>
2024-09-30 19:53:39 +0300

    refactor: Update CLAIMING_ADDRESS and PROOF_ONE in ClaimAirdrop script

--- merkle-airdrop/script/Interact.s.sol
+++ merkle-airdrop/script/Interact.s.sol
@@ -6,13 +6,13 @@ import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
 
 contract ClaimAirdrop is Script {
-    address public CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address public CLAIMING_ADDRESS = 0x67cE8bb909F266AFC7F27BE8a3302C18a7f5Ca14;
     uint256 public CLAIMING_AMOUNT = 25 * 1e18;
-    bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 PROOF_ONE = 0x9458ea849fe18c5a746ca3855ddd0e444ecc1adf5391b75659bc1ae54571acab;
     bytes32 PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] proof = [PROOF_ONE, PROOF_TWO];
     bytes private SIGNATURE =
-        hex"fbd2270e6f23fb5fe9248480c0f4be8a4e9bd77c3ad0b1333cc60b5debc511602a2a06c24085d8d7c038bad84edc53664c8ce0346caeaa3570afec0e61144dc11c";
+        hex"8f317bddf3dfe8c49f3878b3174b54f81970f86548b0ba1b1705c953f7bb5038692fd1b2b715b78526d96ec6b7850c7bde8dd4b27ee39b558d20053deb406bf81b";
 
     error __ClaimAirdropScript__InvalidSignatureLength();
 
