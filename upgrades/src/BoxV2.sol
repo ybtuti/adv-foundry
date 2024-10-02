e22d2647a5940192c93fd1552d1ff6fcc035dc82
ybtuti <samuelmsuu@gmail.com>
2024-10-02 11:15:48 +0300

    feat: Add BoxV1 and BoxV2 contracts
    Add BoxV1.sol and BoxV2.sol contracts to the upgrades/src directory. These contracts implement the Box functionality and are upgradeable using the OpenZeppelin contracts-upgradeable library. BoxV1 is the initial version with basic functionality, while BoxV2 includes an additional setNumber function. Both contracts inherit from OwnableUpgradeable and UUPSUpgradeable.
    
    SPDX-License-Identifier: MIT

---
+++ upgrades/src/BoxV2.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
+
+    function setNumber(uint256 _number) external {}
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 2;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
