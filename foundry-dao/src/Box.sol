c74218986c384bb1ab395bc0b8c28ca02e3aa7a1
ybtuti <samuelmsuu@gmail.com>
2024-10-03 14:46:26 +0300

    feat: Add Makefile for build and deployment automation
    Add a Makefile to automate the build and deployment process in the foundry-dao directory.
    The Makefile includes targets for installing dependencies, building the project, running tests, and creating a snapshot.
    This commit improves the development workflow by providing a standardized way to perform common tasks.

--- foundry-dao/src/Box.sol
+++ foundry-dao/src/Box.sol
@@ -2,4 +2,19 @@
 
 pragma solidity ^0.8.18;
 
-contract Box {}
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private s_number;
+
+    event NumberChanged(uint256 number);
+
+    function store(uint256 newNumber) public onlyOwner {
+        s_number = newNumber;
+        emit NumberChanged(newNumber);
+    }
+
+    function getNumber() external view returns (uint256) {
+        return s_number;
+    }
+}
