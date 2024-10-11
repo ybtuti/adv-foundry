c8db62215abfacc4a3af7b8fa199f5853ad37454
ybtuti <samuelmsuu@gmail.com>
2024-10-11 11:30:29 +0300

    Refactor account-abstraction script files

---
+++ account-abstraction/script/SendPackedUserOp.s.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
+
+contract DeployMinimal is Script {
+    function run() public {}
+    function deployMinimalAccount() public {}
+}
