6f8504128964935da057c09c7379eab695f49154
ybtuti <samuelmsuu@gmail.com>
2024-10-15 22:52:35 +0300

    Refactor .gitignore to include zkout directory

---
+++ account-abstraction/test/zkSync/ZkMinimalAccountTest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+
+contract ZkMinimalAccountTest is Test {
+    function setUp() public {}
+}
+
