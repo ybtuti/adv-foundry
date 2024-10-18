44d528ee31446028db44e4513c6641e05ff49bbb
ybtuti <samuelmsuu@gmail.com>
2024-10-18 13:23:58 +0300

    Refactor ZkMinimalAccountTest to include new test case for validating transactions

--- account-abstraction/test/zkSync/ZkMinimalAccountTest.t.sol
+++ account-abstraction/test/zkSync/ZkMinimalAccountTest.t.sol
@@ -5,19 +5,34 @@ pragma solidity ^0.8.24;
 import {Test} from "forge-std/Test.sol";
 import {ZkMinimalAccount} from "src/zkSync/ZkMinimalAccount.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
-import {Transaction} from
-    "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+
+// Era Imports
+import {
+    Transaction,
+    MemoryTransactionHelper
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {BOOTLOADER_FORMAL_ADDRESS} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {ACCOUNT_VALIDATION_SUCCESS_MAGIC} from
+    "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+
+// Oz Imports
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract ZkMinimalAccountTest is Test {
+    using MessageHashUtils for bytes32;
+
     ZkMinimalAccount minimalAccount;
     ERC20Mock usdc;
 
     uint256 constant AMOUNT = 1e18;
     bytes32 constant EMPTY_BYTES32 = bytes32(0);
+    address constant ANVIL_DEFAULT_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
     function setUp() public {
         minimalAccount = new ZkMinimalAccount();
+        minimalAccount.transferOwnership(ANVIL_DEFAULT_ACCOUNT);
         usdc = new ERC20Mock();
+        vm.deal(address(minimalAccount), AMOUNT);
     }
 
     function testZkOwnerCanExecuteCommands() public {
@@ -37,9 +52,39 @@ contract ZkMinimalAccountTest is Test {
         assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
     }
 
+    function testZkValidateTransaction() public {
+        // Arrange
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+
+        Transaction memory transaction =
+            _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functionData);
+        transaction = _signTransaction(transaction);
+
+        // Act
+        vm.prank(BOOTLOADER_FORMAL_ADDRESS);
+        bytes4 magic = minimalAccount.validateTransaction(EMPTY_BYTES32, EMPTY_BYTES32, transaction);
+        // Assert
+        assertEq(magic, ACCOUNT_VALIDATION_SUCCESS_MAGIC);
+    }
+
     /*//////////////////////////////////////////////////////////////
                                 HELPERS
     //////////////////////////////////////////////////////////////*/
+    function _signTransaction(Transaction memory transaction) internal view returns (Transaction memory) {
+        bytes32 unSignedTransactionHash = MemoryTransactionHelper.encodeHash(transaction);
+        //bytes32 digest = unSignedTransactionHash.toEthSignedMessageHash();
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, unSignedTransactionHash);
+        Transaction memory signedTransaction = transaction;
+        signedTransaction.signature = abi.encodePacked(r, s, v);
+        return signedTransaction;
+    }
+
     function _createUnsignedTransaction(
         address from,
         uint8 transactionType,
